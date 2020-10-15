58c61f811ba44a9b32f80b93067c82cbfe6faa54
Toni <tjimenezsblanca@gmail.com>
2020-10-15 13:30:32 +0200

    trabajo con archivos js de manera básica

--- flaskf/flaskblog/main/routes.py
+++ flaskf/flaskblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, json
 from flaskblog.models import Post
 
 
@@ -9,7 +9,12 @@ main = Blueprint('main',__name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=2)
-    return render_template('home.html', posts = posts, title = 'home')
+    postsJson = Post.query.order_by(Post.date_posted.desc())
+    js = []
+    for post in postsJson:
+        js.append(post.to_dict())
+    
+    return render_template('home.html', posts = posts, json = js, title = 'home')
 
 @main.route('/about')
 def about():
